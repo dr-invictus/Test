75cc1a7a6074cc3433283cbcf35ade8576034b04
ihor <dwerk27@gmail.com>
2022-02-12 18:38:49 +0200

    zadachi

--- script/main.js
+++ script/main.js
@@ -1,1 +1,10 @@
-hello
+//without
+const without = (array, ...args) => {
+    let filteredArray = [...array];
+    for (let i = 0; i < args.length; i += 1) {
+      filteredArray = filteredArray.filter((el) => el !== args[i]);
+    }
+    return filteredArray;
+    }
+  const data = [4, 2, 9, 4, 2];
+  console.log(without(data, 4, 2)); // [9]
